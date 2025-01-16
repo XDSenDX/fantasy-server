3aa285bcc20df166caacebbf00376535145bcae8
XDSenDX <xeseninanutshell@gmail.com>
2025-01-16 08:41:38 +0700

    fix kubejs crash

--- kubejs/startup_scripts/hide stuff.js
+++
@@ -1,9 +0,0 @@
-JEIEvents.hideItems(event => {
-    event.hide('farm_and_charm:supply_cart') ///hide cart
-  });
-JEIEvents.hideItems(event => {
-    event.hide('farm_and_charm:plow') ///hide plow
- });
- JEIEvents.hideItems(event => {
-    event.hide('meadow:watering_can') ///hide watering can
- });
